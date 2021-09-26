d74f5a1f8a0e34fb74956d1aab8f8811d41e0e59
KotaGit <Kota7arai@gmail.com>
2021-09-26 23:22:26 +0900

    ルート移動、プレイヤーの移動可能
    ルート移動、プレイヤーの移動可能

--- Assets/Scripes/PlayerCtrl.cs
+++ Assets/Scripes/PlayerCtrl.cs
@@ -9,11 +9,17 @@ public class PlayerCtrl : MonoBehaviour
     [Range(0, 200)]
     public float Speed = 10.0f;
 
+    [Range(0, 200)]
+    public float MoveSpeed = 10.0f;
+    public float MoveRange = 40.0f;
+
     bool _isHitRoutePoint;
 
     IEnumerator Move()
     {
         var prevPointPos = transform.position;
+        var basePosition = transform.position;
+        var movedPos = Vector2.zero;
 
         foreach (var nextPoint in RoutePoints)
         {
@@ -26,7 +32,19 @@ public class PlayerCtrl : MonoBehaviour
                 vec.Normalize();
 
                 //プレイヤーの移動
-                transform.position += vec * Speed * Time.deltaTime;
+                //transform.position += vec * Speed * Time.deltaTime;
+                basePosition += vec * Speed * Time.deltaTime;
+
+                //上下左右に移動する処理
+                movedPos.x += Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+                movedPos.y += Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+                movedPos = Vector2.ClampMagnitude(movedPos, MoveRange);
+                var worldMovedPos = Matrix4x4.Rotate(transform.rotation).MultiplyVector(movedPos);
+
+                //ルート上に位置に上下殺宇野移動量を加える
+                transform.position = basePosition + worldMovedPos;
+
+
                 //進行方向を向くように計算する
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(vec, Vector3.up), 0.5f);
 
