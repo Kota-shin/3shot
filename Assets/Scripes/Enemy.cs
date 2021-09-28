01e513b5f0e8a8e746cf989d6679a5eb1601aec6
KotaGit <Kota7arai@gmail.com>
2021-09-28 22:49:06 +0900

    敵スクリプトまで
    敵スクリプトまで

---
+++ Assets/Scripes/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Range(0, 100)]
+    public float Speed = 10f;
+    public float DeadSecond = 10f;
+
+    float _time;
+    Player _player;
+    void Start()
+    {
+        _time = 0f;
+        _player = Object.FindObjectOfType<Player>();
+    }
+
+
+    void Update()
+    {
+        _time += Time.deltaTime;
+        if(_time >= DeadSecond)
+        {
+            Object.Destroy(gameObject);
+        }
+        else
+        {
+            var vec = _player.transform.positon - transform.positon;
+            transform.position += vec.normalized * Speed * Time.deltaTime;
+        }
+    }
+}
